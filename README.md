a14ddd24f7f001e49aa5d57d670b1dbfbed3e9c3
keifer-may <73715008+keifer-may@users.noreply.github.com>
2025-11-30 21:23:31 -0500

    Refactor notes into the README instead of the init.lua

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# my_nvim_config
-Building this up
+# MY NEOVIM CONFIG
+
+Trying to build this up from scratch in a fairly minimal style, not a ton of dependencies or BS and to also document the few dependencies and things I do need all in one centralized spot.
+
+## NOTE TO SELF
+
+If you decide maintaining this is too much, check out:
+- https://github.com/junegunn/vim-plug (some old school vim plug-in management)
+- https://github.com/mason-org/mason.nvim (for lsp junk)
+- https://github.com/nvim-lua/kickstart.nvim (the og kickstart)
+- https://github.com/keifer-may/kickstart.nvim (my personal kickstart)
+- https://www.lazyvim.org/
+- https://www.lunarvim.org/
+
+## GENERAL DEPENDENCIES
+- Install *xclip* for X11 or *wl-clipboard* for Wayland to have access to the system clipboard between OS & Neovim
+- *homebrew*
+- *npm*
+- *python3*
+- *pip* or *pipx*
+- *go*
+
+## VIM.PACK NOTES
+
+First we install different colorschemes for themes.
+To find more visit: https://vimcolorschemes.com/
+
+Following the colorschemes we install (in order):
+- *DEPRECATED* NVIM Oil
+    - For more info on Oil go to: https://github.com/stevearc/oil.nvim
+    - Oil is a file explorer that lets you edit your filesystem like a nvim buffer
+
+- NVIM Mini
+    - Maybe checkout more Mini tools or info?: https://nvim-mini.org/mini.nvim/
+    - We'll use this to get some file fuzzy finding, general file exploring,
+    autocomplete integration, shortcuts for commenting code, some around
+    and inside tooling, and general appearance improvement.
+
+- NVIM LSP Config,
+    - The github: https://github.com/neovim/nvim-lspconfig
+    - Some examples and junk for lsp setups: https://neovim.io/doc/user/lsp.html#lsp-quickstart
+
+- Typst Preview
+    - Get an auto updating preview of your html site
+    - https://github.com/chomosuke/typst-preview.nvim
+
+## LSP NOTES
+
+#### GENERAL NOTES OR TROUBLESHOOTING
+
+If you need to refresh your file's formatting and refresh the file LSP:
+
+```
+<leader>lf 
+```
+
+You may also need to save, source the config init.lua, and then run in bash to refresh bash:
+
+```
+exec bash
+```
+
+To get a little more insight on which LSPs are working or are having issues you can run in vim:
+
+```
+:checkhealth vim.lsp
+```
+
+#### LUA-LANGUAGE-SERVER
+
+- For lua_ls go to: https://luals.github.io/#neovim-install
+- Install homebrew at: https://brew.sh/ running the command:
+    ```
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    ```
+- Then run the command:
+    ```
+    brew install lua-language-server
+    ```
+
+#### PYLSP
+
+- Obviously, Python3 & pip should be installed. Run:
+```
+pip install python-lsp-server
+```
+
+#### GOPLS
+
+- For gopls go to: https://go.dev/gopls/
+- Ensure that go is installed and GOPATH is set up in your ENV then run:
+```
+go install golang.org/x/tools/gopls@latest
+```
+
+#### CCLS
+
+- to note....
+
+#### RUST-ANALYZER
+
+- to note....
+
+#### EMMET-LS
+
+- For emmet-ls go to: https://github.com/aca/emmet-ls
+- Ensure that node is installed and up-to-date and then run:
+```
+npm install -g emmet-ls
+``` 
+
+
+
--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 local vim = vim
 
 -- Begin by setting general vim options:
--- vim.cmd("set clipboard+=unnamedplus")
--- !Make sure you have xclip or something like it installed!
 vim.o.clipboard = 'unnamed,unnamedplus'
 vim.o.background = 'dark'
 vim.o.signcolumn = 'yes'
@@ -17,13 +15,7 @@ vim.o.scrolloff = 30
 vim.o.swapfile = false
 vim.o.winborder = 'rounded'
 
---[[
-
-    Set the map leader key
-    When in normal mode, use space to get special keymaps we'll be setting
-
-]]--
-
+-- Set the map leader key; use space to get special keymaps we'll be setting.
 vim.g.mapleader = ' '
 
 -- Set some keymaps for leader keys that will source, write to the file, and quit the file
@@ -31,38 +23,7 @@ vim.keymap.set('n', '<leader>u', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
---[[
-
-    Use pack to install the few things I do want to use as addons
-    If you feel you need to give up, checkout:
-        https://github.com/junegunn/vim-plug (some old school vim plug-in management)
-        https://github.com/mason-org/mason.nvim (for lsp junk)
-        https://github.com/nvim-lua/kickstart.nvim (the og kickstart)
-        https://github.com/keifer-may/kickstart.nvim (my personal kickstart)
-        https://www.lazyvim.org/
-        https://www.lunarvim.org/
-
-    First we are installing different colorschemes for themes
-        To find more visit: https://vimcolorschemes.com/
-
-    Following the colorschemes we install (in order):
-        - NVIM Oil
-            For more info on Oil go to: https://github.com/stevearc/oil.nvim
-            Oil is a file explorer that lets you edit your filesystem like a nvim buffer
-
-        - NVIM Mini,
-            Maybe checkout more Mini tools or info?: https://nvim-mini.org/mini.nvim/
-
-        - NVIM LSP Config,
-            The github: https://github.com/neovim/nvim-lspconfig
-            Some examples and junk for lsp setups: https://neovim.io/doc/user/lsp.html#lsp-quickstart
-
-        - Typst Preview
-            Get an auto updating preview of your html site
-            https://github.com/chomosuke/typst-preview.nvim
-
-]]--
-
+-- Use vim.pack to install the few things I do want to use as addons. To see more, check the README.
 vim.pack.add({
     { src = 'https://github.com/vague-theme/vague.nvim' },
     { src = 'https://github.com/sainnhe/everforest' },
@@ -79,16 +40,16 @@ vim.pack.add({
     --{src = ''},
 })
 
--- Add LSP autocomplete
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotriger = true })
-        end
-    end,
-})
-vim.cmd('set completeopt+=noselect')
+--Add LSP autocomplete
+--vim.api.nvim_create_autocmd('LspAttach', {
+--     callback = function(ev)
+--         local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--         if client:supports_method('textDocument/completion') then
+--             vim.lsp.completion.enable(true, client.id, ev.buf, { autotriger = true })
+--         end
+--     end,
+-- })
+-- vim.cmd('set completeopt+=noselect')
 
 -- Require the NVIM Mini-Pick and NVIM Oil are setup
 require 'mini.pick'.setup() -- Provides a fuzzy finder/buffer search for files
@@ -113,16 +74,12 @@ vim.lsp.config['lua_ls'] = {
     cmd = { 'lua-language-server' },
     -- Filetypes to automatically attach to.
     filetypes = { 'lua' },
-    --[[
-        Sets the "workspace" to the directory where any of these files is found.
-        Files that share a root directory will reuse the LSP server connection.
-        Nested lists indicate equal priority, see |vim.lsp.Config|.
-    ]]--
+        -- Sets the "workspace" to the directory where any of these files is found.
+        -- Files that share a root directory will reuse the LSP server connection.
+        -- Nested lists indicate equal priority, see |vim.lsp.Config|.
     root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-    --[[
-        Specific settings to send to the server. The schema is server-defined.
-        Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-    ]]--
+        -- Specific settings to send to the server. The schema is server-defined.
+        -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
     settings = {
         Lua = {
             runtime = {
@@ -130,7 +87,6 @@ vim.lsp.config['lua_ls'] = {
             }
         }
     }
-
 }
 
 -- Setup the emmet_ls LSP (pain in the ass too)
@@ -150,35 +106,7 @@ vim.lsp.config['emmet-ls'] = {
     }
 }
 
---[[
-
-    Enable the different LSPs that I've installed
-    Notes on installs:
-    --------------------------------------------------
-    --- If you have some general issues or questions:
-    --- run <leader>lf to see if lsp formatting will refresh
-    --- if that doesn't work, try:
-    --- :checkhealth vim.lsp
-    --------------------------------------------------
-    For lua_ls go to: https://luals.github.io/#neovim-install
-    Install homebrew at: https://brew.sh/ 
-    running the command:
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    Then run the command:
-        brew install lua-language-server
-    --------------------------------------------------
-    For emmet-ls go to: https://github.com/aca/emmet-ls
-    Ensure that node is installed and up-to-date and then run:
-    npm install -g emmet-ls
-    --------------------------------------------------
-    For gopls go to: https://go.dev/gopls/
-    Ensure that go is installed and GOPATH is set up in your ENV then run:
-    go install golang.org/x/tools/gopls@latest
-    --------------------------------------------------
-
-
-]]--
-
+-- Enable the different LSPs that I've installed. For install info, see the README.
 vim.lsp.enable({ 'lua_ls', 'pylsp', 'gopls', 'ccls', 'rust_analyzer', 'emmet-ls' })
 
 -- Setting the actual colorscheme from the earlier installed colorscheme
@@ -189,26 +117,4 @@ vim.cmd('colorscheme iceberg')
 --vim.cmd('colorscheme everforest')
 --vim.cmd('colorscheme vague')
 
---[[
---
--- Some other things to learn about and try:
---
--- Need a good debugger if I'm learning to program in C!
---
--- For Windows, just use the raddebugger!:
--- https://github.com/EpicGamesExt/raddebugger/ && https://github.com/EpicGamesExt/raddebugger/releases
--- Failing that, RemedyBG is pretty awesome looking:
--- https://remedybg.itch.io/remedybg
--- A RemedyBG tutorial by Casey Muratori: https://www.youtube.com/watch?v=r9eQth4Q5jg
---
--- For Linux you need to install GDB:
--- docs: https://sourceware.org/gdb/current/onlinedocs/gdb
---
--- Then try Seer:
--- https://github.com/epasveer/seer
--- or try GF:
--- https://github.com/nakst/gf
--- A fun debugging the debugger video: https://www.youtube.com/watch?v=ux3s1IIvNdg
---
-]]--
 
